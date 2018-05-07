a04ab2a8578ea901c0fa8cddb8c139ec3d3001b8
Daria Sirous <dsirous@tribalscale.com>
2018-05-07 15:12:16 -0400

    [DS/JS] - Initial commit
    Signed-off-by: Jaeho Sim <jsim@tribalscale.com>

---
+++ app.js
@@ -0,0 +1,5 @@
+const command = require('yargs').argv._[0];
+
+const COMMANDS = require('./src/constants.js').COMMANDS;
+
+console.log(COMMANDS);
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+const COMMANDS = {
+  ADD: 'ADD',
+  LIST: 'LIST',
+  READ: 'READ',
+  REMOVE: 'REMOVE',
+};
+
+module.exports = {
+  COMMANDS,
+};
